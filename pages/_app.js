48f7ae23e0c7e315843d26ecd0bdfe22b016efd0
Jaisartuyk <jairosancheztriana@gmail.com>
2026-04-24 11:40:13 -0500

    Convert to PWA: added manifest, sw, and icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then((registration) => {
+          console.log('SW registrado con éxito:', registration);
+        }).catch((error) => {
+          console.log('Fallo registro de SW:', error);
+        });
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
